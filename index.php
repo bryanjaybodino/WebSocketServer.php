6f35c58fc80638f0d011cfc471d1efce41fd4ef0
Bryanjay Bodino <bryanjaybodino@gmail.com>
2024-08-27 16:39:04 +0800

    query string filter

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 include_once "WebSocketServer.php";
 
+echo "ws://127.0.0.1:8090"; // Default
 
-echo "ws://127.0.0.1:8090<br>";
-echo "ws://localhost:8090<br>";
-echo "ws://{Your IPv4}:8090";
+// Add Room for selecting client to receive specific echo back message
+echo "ws://127.0.0.1:8090?room=1";
+echo "ws://127.0.0.1:8090?room=2";
+echo "ws://127.0.0.1:8090?anyname=2";
 
 // Usage
 $server = new WebSocketServer(8090);
